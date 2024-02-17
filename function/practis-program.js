8cce6040ce74545f61125cfe78f765b656f725a1
nikhil kalsariya <nickahir0786@gmail.com>
2024-02-17 08:51:04 +0530

    basic

---
+++ function/practis-program.js
@@ -0,0 +1,104 @@
+// task-1
+
+// add(10, 5);
+// function add(a, b) {
+// document.write(a + b);
+// }
+
+
+
+// task-2
+
+// add(100, 2);
+// function add(a, b) {
+//     document.write(a * b);
+// }
+
+
+
+// task-3
+
+// add(100, 5);
+// function add(a, b) {
+//     document.write(a - b);
+// }
+
+
+
+// task-4
+
+// let product = function (x , y){
+//     return x *  y;
+// };
+// document.write(product(100,2));
+
+
+
+// task-5
+
+// let x = (function hellow (a , b)
+// {
+//     return (a - b);
+// })( 100 , 50);
+// document.write(x)
+
+
+
+// // task-6
+// function add(a, b) {
+//     return a + b;
+// }
+// let z = add(10, 15)
+// console.log(z);
+
+
+
+// // task-7
+
+// function squre(a , b){
+//     console.log(a*b);
+// }
+// squre(10 ,5)
+
+
+
+// task-8
+
+//  let fact = (n) =>
+// {
+//     if(n == 1)
+//     {
+//         return 1;
+//     }
+//     else
+//     return n * fact(n-1);
+// }
+// let x = fact(5);
+// console.log("factorial is:" + x);
+
+// task-9
+
+// let newfun = (n) =>{
+//     console.log("winding phase :" + n);
+//     if(n < 3){
+//         newfun(++n);
+//     }
+//     console.log("unwinding phase:" + n);
+// }
+// console.log((newfun(2)));
+
+
+// task-10
+
+//  function Mult(x) {
+//         for (let i = 1; i <= 10; i++) {
+//             console.log(i * x);
+//             x = x * 2;
+//         }
+//         return x;
+//     }
+//     console.log(Mult(1))
+
+// task-11
+
+
